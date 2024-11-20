f62cf76bc6c67682d7178af2b9643a27b9442e01
Nathan Watkins <nathan.wtkns@gmail.com>
2024-11-20 18:20:38 +0000

    Fixed vertical scroll
    Fixed issue with the cast list where the list would not go back to the start for the next movie

--- components/Cast.tsx
+++ components/Cast.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import CastPoster from "./castPoster";
+import React, { useEffect, useRef } from "react";
+import CastPoster from "./CastPoster";
 import { ScrollView } from "react-native";
 
 const Cast = ({ cast }: { cast: any }) => {
   const MAX_CAST_MEMBERS = 15;
 
   const limitedCast = cast.slice(0, MAX_CAST_MEMBERS);
+
+  const scrollViewRef = useRef<ScrollView>(null);
+  useEffect(() => {
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: true });
+    }
+  }, [cast]);
+
   return (
-    <ScrollView horizontal={true} snapToInterval={160}>
+    <ScrollView horizontal={true} snapToInterval={160} ref={scrollViewRef}>
       {limitedCast.map((person: any, index: number) => (
         <CastPoster key={index} person={person} />
       ))}
